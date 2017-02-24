56700d34d7e2b4ea3b42f3dfdda525d7102d0a7e
adhelhgi <lhgi.fahmy@yahoo.com>
2017-02-24 22:53:06 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,17 @@
 <?php
-$ldap_dn = "cn=admin,dc=tata,dc=com";
-$ldap_dn2 = "cn=user-group,ou=user,dc=tata,dc=com";
+$ldap_dn = "cn=admin,dc=openldap,dc=com";
+$ldap_dn2 = "cn=user-group,ou=user,dc=openldap,dc=com";
 $ldap_pass = "jangkrik";
 $dn = "dc=tata,dc=com";
 
-$ldap_con = ldap_connect("10.0.2.15",389);
+$ldap_con = ldap_connect("192.168.100.1",389);
 ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
 
 
 
 $host = "localhost";
-$user = "root";
-$pass = "jangkrik";
+$user = "radius";
+$pass = "mysqlsecret";
 $name = "radius";
  
 $koneksi = mysqli_connect($host, $user, $pass, $name) ;
